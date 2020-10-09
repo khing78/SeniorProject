08d790d89c738e2e16a9ffd0a65353cbf700d207
ThanawatPermngram <thanawatpar@kkumail.com>
2020-10-09 23:56:54 +0700

    build some pindetail

---
+++ Client/cassava/src/views/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar color="white" dense dark>
+      <v-toolbar-title style="margin-top: 0px; color:#1CE227" class="navbar">ระบบแสดงคุณภาพมันสำปะหลัง</v-toolbar-title>
+      <v-spacer/>
+      <a id="username" style="color:#1CE227; padding-right: 5px">ผู้ใช้งาน1</a>
+      <v-avatar><v-img src="../assets/iconusercc.png"></v-img></v-avatar>
+    </v-app-bar>
+  </div>
+</template>
+
+<style>
+  .navbar{
+    font-family: "Prompt", sans-serif;
+  }
+</style>
