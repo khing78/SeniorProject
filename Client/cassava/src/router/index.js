83efd425be869167496c62a59418a8a7e8f92e78
ThanawatPermngram <thanawatpar@kkumail.com>
2020-10-09 22:43:23 +0700

    Set Router

--- Client/cassava/src/router/index.js
+++ Client/cassava/src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PinDetail from '../views/PinDetail.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
+  },
+  {
+    path: '/pin-detail',
+    name: 'pinDetail',
+    component: PinDetail
   },
   {
     path: '/about',
