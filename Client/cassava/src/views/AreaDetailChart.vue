9bb4269f2b79b291daf3fbadce8af3ad7bca3dcc
ThanawatPermngram <thanawatpar@kkumail.com>
2020-10-24 12:36:22 +0700

    areadetail finished

--- Client/cassava/src/views/AreaDetailChart.vue
+++ Client/cassava/src/views/AreaDetailChart.vue
@@ -6,7 +6,7 @@
         <v-col class="text-right">
         </v-col>
       </v-row>
-      <v-row>
+      <v-row >
           <v-col cols="9">
             <canvas id="myChart" width="400" height="200"></canvas>
               <!-- For Graph -->
@@ -38,39 +38,18 @@ window.addEventListener('load', function() {
 var ctx = document.getElementById('myChart');
 // eslint-disable-next-line no-unused-vars
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','x','x','x','x','x'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: [12, 19, 3, 5, 2, 3,2,3,6,4,6],
+            borderColor: 'rgba(0, 0, 0, 1)',
+            fill: false,
         }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+        
     }
 });
 });
