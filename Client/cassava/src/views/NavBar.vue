993bc263f1cb53d19fb01a0f954f4b11dd424066
ThanawatPermngram <thanawatpar@kkumail.com>
2020-12-16 19:01:18 +0700

    Logout Button

--- Client/cassava/src/views/NavBar.vue
+++ Client/cassava/src/views/NavBar.vue
@@ -5,26 +5,45 @@
         >ระบบแสดงคุณภาพมันสำปะหลัง</v-toolbar-title
       >
       <v-spacer />
-      <a id="username" style="color: #1ce227; padding-right: 5px">{{
-        name
-      }}</a>
+      <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+        color="white"
+          v-bind="attrs"
+          v-on="on"
+          text
+          :disabled="!name"
+        >
+          <div id="username" style="color: #1ce227; padding-right: 5px">{{ name }}</div>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+        >
+          <v-list-item-content @click="logoutbutton()">Logout</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
       <v-avatar><v-img id="imagecharacter" :src="imagesrc"></v-img></v-avatar>
     </v-app-bar>
   </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 export default {
   data: () => ({
     usernameA: "",
     imagesrc: "../assets/iconusercc.png",
   }),
   methods: {
+    logoutbutton(){
+
+    }
   },
   computed: {
     ...mapGetters({
-      name: "getName"
-    })
+      name: "getName",
+    }),
   },
 };
 </script>
